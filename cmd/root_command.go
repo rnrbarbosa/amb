4cf9d6a6a307992439c4b9df787af14b9ff09036
Roberto Barbosa <rnrbarbosa@gmail.com>
2018-11-04 20:59:56 +0100

    rename tool to 3 letters, and root cmd file

---
+++ cmd/root_command.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var (
+	// VERSION is set during build
+	VERSION string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "amb",
+	Short: "An Ambari Command Line Tool",
+	Long:  `GoAmbari is a command line tool for Cluster Configuration.`,
+
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func Execute(version string) {
+	VERSION = version
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.goambari.yaml)")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".goambari" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".goambari")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
